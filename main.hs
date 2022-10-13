f93ec225f588c2bc5cc8c1a8b3e3e63f420b08da
Lucas Sousa <lcsferreiradesousa@hotmail.com>
2022-10-13 10:16:39 +0100

    adding polynomials

--- main.hs
+++ main.hs
@@ -1,5 +1,27 @@
+import Data.List
 type Nomial = (Int, [(Char, Int)])
 type Polynomial = [Nomial]
 
-addNomial :: Nomial -> Nomial
-addNomial n1 n2 = if n1.snd == n2.snd then Nomial(n1.fst + n2.fst, n1.snd)
+--canonycal:: Polynomial -> Polynomial
+--canonycal 
+
+addNomial:: Nomial -> Nomial -> Nomial
+addNomial a b = (fst a + fst b, snd a)
+
+add :: Polynomial -> Polynomial -> Polynomial
+add [a] b = addAux a b
+add (a:as) b = addAux a (add as b) 
+
+
+addAux:: Nomial -> Polynomial -> Polynomial
+addAux a [] = [a]
+addAux a (b:bs) = if snd a == snd b then addNomial a b : bs
+                else b: addAux a bs
+
+
+--multiplyNomial:: Nomial -> Nomial -> Nomial
+--multiplyNomial a b = (fst a * fst b, c)
+--                    where c = nub [()]
+
+-- [(1, [('x', 1)]), (2, [('x', 2)])]
+-- [(1, [('x', 2)]), (2, [('x', 3)])]
