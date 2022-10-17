485bd08eb6ff703ef0c49dd499d38ed7449d5bdc
Lucas Sousa <lcsferreiradesousa@hotmail.com>
2022-10-17 19:24:50 +0100

    remove all 0 exponetns

--- main.hs
+++ main.hs
@@ -12,13 +12,13 @@ add [a] b = addAux a b
 add (a:as) b = addAux a (add as b) 
 
 normalize:: Polynomial -> Polynomial
-normalize a = add (pssort (sortAllNomials a)) []
--- usar add polinomio [] para somar monomoios do mesmo polinomio
+normalize a = add (pssort (map nssort (remove0coeficients (map remove0exponents a)))) []
 
 -- ======================================================
 --                  Aux Functions
 -- ======================================================
 
+-- add
 addNomial:: Nomial -> Nomial -> Nomial
 addNomial a b = (fst a + fst b, snd a)
 
@@ -26,27 +26,42 @@ addAux:: Nomial -> Polynomial -> Polynomial
 addAux a [] = [a]
 addAux a (b:bs) = if snd a == snd b then addNomial a b : bs
                 else b: addAux a bs
+
+-- normalize
+    -- polynomial sort
 pMin :: Polynomial -> Nomial
 pMin [x] = x
 pMin (x:xs) = if snd x <= snd (pMin xs) then x else pMin xs
 
 pssort:: Polynomial -> Polynomial
 pssort [] = []
-pssort a = m : pssort (delete m a)
+pssort a = m : pssort (Data.List.delete m a)
     where m = pMin a
 
+    -- monomial sort
 nssort :: Nomial -> Nomial
-nssort (a, x) = (a, reverse (myssort x))
+nssort (a, x) = (a, Data.List.reverse (myssort x))
 
 myssort:: [(Char, Int)] -> [(Char, Int)]
 myssort [] = []
-myssort a = m : myssort (delete m a)
+myssort a = m : myssort (Data.List.delete m a)
     where m = nMin a
 
 nMin :: [(Char, Int)] -> (Char, Int) 
 nMin [x] = x
 nMin (x:xs) = if snd x <= snd (nMin xs) then x else nMin xs
 
-sortAllNomials:: Polynomial -> Polynomial
-sortAllNomials [x] = [x]
-sortAllNomials (x: xs) = nssort x : sortAllNomials xs 
+    -- remove 0x^1
+remove0coeficients:: Polynomial -> Polynomial
+remove0coeficients [] = []
+remove0coeficients (x:xs) = if fst x == 0 then remove0coeficients xs
+                            else x : remove0coeficients xs 
+    --change 2x^0 to 2
+remove0exponents:: Nomial -> Nomial
+remove0exponents (a, x) = (a, remove0exponentsAux x)
+
+remove0exponentsAux:: [(Char, Int)] -> [(Char, Int)]
+remove0exponentsAux [] = []
+remove0exponentsAux (x:xs) = if snd x == 0 then remove0exponentsAux xs
+                            else x: remove0exponentsAux xs
+
