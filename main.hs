aebf7c85e3f124bceb468daeb9e76df26c8ae458
Andrekt02 <andremiguelpachecomorais@gmail.com>
2022-10-19 10:30:03 +0100

    multiply and working derivate

--- main.hs
+++ main.hs
@@ -11,27 +11,28 @@ type Polynomial = [Nomial]
 --Example broken:
 --derivated do exemplo normalizado
 
-
 -- ======================================================
 --                    String Parsing
 -- ===================================================
--- TO DO --
+--ignorar espaços procurar "+"" ou "-" para separar 
+--depois de encontrar um desses símbolos tem de aparecer o numero que é coeficiente e depois  
 
 -- ======================================================
 --                  Main Functions
 -- ======================================================
 
 add:: Polynomial -> Polynomial -> Polynomial
---add a b = normalize (addBeforaNormalize a b)
-add [a] b = addAux a b
-add (a:as) b = addAux a (add as b)
+add a b = normalize (addBeforeNormalize a b)
 
 normalize:: Polynomial -> Polynomial
---normalize a = reverse (myisort (assort (addBeforaNormalize (map (nisort . nssort) ( remove0coeficients (map remove0exponents a))) [] ))) --not working
-normalize a = reverse (myisort (assort (add (map nssort ( remove0coeficients (map remove0exponents a))) [])))
+normalize a = reverse (myisort (assort (addBeforeNormalize (map (nisort . nssort) ( remove0coeficients (map remove0exponents a))) [] ))) --not working
 
-derivate:: Polynomial -> Polynomial
-derivate = map derivateNomial
+--derivate:: Polynomial -> Char -> Polynomial --pedir para derivar em ordem a x
+--derivate a x = map derivateNomial normalize(a) x
+
+multiply:: Polynomial -> Polynomial ->Polynomial
+multiply a b = normalize (multiplyBefore a b)
+
 
 -- need to sort alphabeticaly
 
@@ -39,18 +40,19 @@ derivate = map derivateNomial
 --                  Aux Functions
 -- ======================================================
 
+-- =================== derivate ==========================
+--normalizo primeiro e depois calculo  --percorro a lista ate encontrar a variavel pedida e depois aplico aí o derivate nomial para essa e junto o resto
+--derivateNomial:: Nomial -> Char -> Nomial --derivar com apenas uma variavel (para fazer com varias variaveis tinhamos de perguntar em ordem a x,y,z...)  
+--derivateNomial (_, []) _ = (0,[]); --derivada de 2 em ordem a qualquer coisa é 0 (derivada sem variaveis)
+--derivateNomial (coef,[(v,ex)]) x = if v == x then (coef * ex,[(v,ex-1)]) else (0,[]) --(derivada com 1 variavel) derivada de 2x^2 a x é 4x e a y é 0
+--derivateNomial (coef,l) x = (coef,derivateNomialList l) --derivada com mais do que 1 variavel
+--derivateNomial (coef,a:as) x = if fst a == x then addtuple ((derivateNomial (coef,[a])) (coef,as)) else (derivateNomial (coef,as) : coef,a))
+
 -- ======================= add ==========================
 
-addBeforaNormalize :: Polynomial -> Polynomial -> Polynomial
-addBeforaNormalize a [] = a
-addBeforaNormalize [] a = a
-addBeforaNormalize [a] b = addAux a b
-addBeforaNormalize (a:as) b = addAux a (addBeforaNormalize as b)
-
-derivateNomial:: Nomial -> Nomial --derivar com apenas uma variavel (para fazer com varias variaveis tinhamos de perguntar em ordem a x,y,z...)  
-derivateNomial (_, []) = (0,[]);
-derivateNomial (coef,[(v,ex)]) = (coef * ex,[(v,ex-1)])
-derivateNomial (_,_) = error "multiple variables"
+addBeforeNormalize :: Polynomial -> Polynomial -> Polynomial
+addBeforeNormalize [a] b = addAux a b
+addBeforeNormalize (a:as) b = addAux a (addBeforeNormalize as b)
 
 addNomial:: Nomial -> Nomial -> Nomial
 addNomial a b = (fst a + fst b, snd a)
@@ -58,18 +60,29 @@ addNomial a b = (fst a + fst b, snd a)
 addAux:: Nomial -> Polynomial -> Polynomial
 addAux a [] = [a]
 addAux a (b:bs) = if snd a == snd b then addNomial a b : bs
-                  else b: addAux a bs
+                else b: addAux a bs
 
---multiply nomials
---multiplyNomial:: Nomial -> Nomial -> Int
---multiplyNomial a b = (fst a * fst b, if(fst(head(fst a)) == fst(head(fst b))) then (snd(head(snd a) + snd(head(snd b)))) else "tenho de acabar isto")
-                                              --multiplicamos sempre o coeficiente e depois
-                                              --temos de ver se char for igual -> somar ints e manter o char
-                                              -- se char for diferente -> juntar chars (por ordem alfabetica)
-                                              -- fazer isso para a lista toda (é a parte mais tricky eu acho)
-                                              --snd(head(snd (7, [('y', 2)]))) -> 2
+-- ===================Multiply ==========================
 
+-- [(1, [('x', 1)]), (2, [('y', 1)])] [(3, [('x', 1)]), (-4, [('y', 1)]), (5, [])]
 
+multiplyBefore:: Polynomial -> Polynomial -> Polynomial
+multiplyBefore [] b = []
+multiplyBefore (a: as) b = addBeforeNormalize (multiplyAux a b) (multiplyBefore as b)
+
+multiplyAux:: Nomial -> Polynomial -> Polynomial
+multiplyAux a = map (nMulti a)
+
+nMulti:: Nomial -> Nomial -> Nomial
+nMulti a b = ((fst a)*(fst b), nMultiAux (snd a) (snd b));
+
+nMultiAux :: [(Char, Int)] -> [(Char, Int)] -> [(Char, Int)]
+nMultiAux as b = foldr nMultiAux2 b as
+
+nMultiAux2 :: (Char, Int) -> [(Char, Int)] -> [(Char, Int)]
+nMultiAux2 a [] = [a]
+nMultiAux2 a (b: bs) = if(fst a == fst b) then (fst a, snd a + snd b) : bs
+            else b: nMultiAux2 a bs
 -- ==================== Normalize =======================
 
     -- polynomial alphabetical sort
